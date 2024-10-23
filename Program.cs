73de19acdb6fe5cded7c753aa73d4ca73f9b3dce
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-10-23 11:07:59 +0200

    Array

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*static void Main(){
+    int[] a = new int[5];
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        Console.Write("Tal nr " + (i+1) + ": ");
+        a[i] = int.Parse(Console.ReadLine());
+    }
+    Console.Write( (a[4], a[3], a[2], a[1], a[0]));
+}
+Main();
+
+static void Main()
+{
+    double[] temperatur = { 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9};
+    double a = 0;
+    for (int i = 0; i < temperatur.GetLength(0); i++)
+    {
+        a += temperatur[i];
+    }
+    double b = Math.Round(a/temperatur.GetLength(0), 2);
+    Console.WriteLine(b);
+}
+Main();
+*/
+

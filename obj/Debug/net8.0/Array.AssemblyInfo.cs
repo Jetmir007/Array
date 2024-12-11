54156cf348cc0268c6ad09d705c3b5b95447e5d4
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-12-11 10:20:59 +0100

    array

--- obj/Debug/net8.0/Array.AssemblyInfo.cs
+++ obj/Debug/net8.0/Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24234a991365459f2421ea38ec15a421d4a7e1a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e937487fa1c452b053955c2ca65477539db8274d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
